e806c0231be6af8007249167ed75f9645b12789f
Weiwen-Hong <weiwenhong9812@gmail.com>
2018-05-05 14:16:45 -0700

    ui done

--- hackathon/src/app/app.component.ts
+++ hackathon/src/app/app.component.ts
@@ -7,7 +7,32 @@ import { Routes, RouterModule } from "@angular/router";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
   firstName: string = '';
   lastName: string = '';
+  day: string = '0';
+  month: string = '0';
+  year: string = "0";
+  DOB: string = "";
+  phone: number = 0;
+  filled: boolean = false;
+  token: string = "q5xjzzabe8tjhdz3wwwcqgrq";
+
+  dayHandler(event: Event) {
+    this.day = (<HTMLInputElement>event.currentTarget).value;
+  };
+
+  monthHandler(event: Event) {
+    this.month = (<HTMLInputElement>event.currentTarget).value;
+  };
+
+  yearHandler(event: Event) {
+    this.year = (<HTMLInputElement>event.currentTarget).value;
+  };
+
+
+  setupPatient() {
+    this.DOB = this.month + "/" + this.day + "/" + this.year;
+
+  };
+
 }
