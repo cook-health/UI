1214578dcfb8736d38e65a65a1cff012a1a6a01e
Weiwen-Hong <weiwenhong9812@gmail.com>
2018-05-05 21:38:13 -0700

    textfield more ui

--- hackathon/src/app/documentation/documentation.component.ts
+++ hackathon/src/app/documentation/documentation.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentationComponent implements OnInit {
 
+  symptoms: string = "";
+  diagnosis: string = "";
+  treatment: string = "";
   constructor() { }
 
   ngOnInit() {
